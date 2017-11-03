02073d2f7a395dcbdcdd4f737ef0fc815f6b0dd0
sandrakuc <san.kuc.96@gmail.com>
2017-11-03 17:56:29 +0100

    pierwsze pliki

---
+++ Engine.h
@@ -0,0 +1,16 @@
+#ifndef ENGINE_H_INCLUDED
+#define ENGINE_H_INCLUDED
+
+class Engine
+{
+    public:
+        Engine();
+        virtual ~Engine();
+        int initiall();
+    protected:
+
+    private:
+};
+
+
+#endif // ENGINE_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <allegro.h>
+#include "Engine.h"
+#define WIDTH 1600
+#define HEIGHT 900
+
+using namespace std;
+
+
+int main(){
+Engine engine;
+engine.initiall();
+while
+( (readkey()& 0xff) != 27);
+allegro_exit();
+return 0;
+}
+END_OF_MAIN()
+
