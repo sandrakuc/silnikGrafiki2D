f4f95d20241cdf4e520c80e008f867901ead541a
sandrakuc <san.kuc.96@gmail.com>
2017-11-12 18:43:50 +0100

    LineSegment

---
+++ LineSegment.cpp
@@ -0,0 +1,5 @@
+#include <allegro.h>
+#include <vector>
+#include "Point2D.h"
+#include "Engine.h"
+#include "LineSegment.h"
---
+++ LineSegment.h
@@ -0,0 +1,16 @@
+#ifndef LINESEGMENT_H_INCLUDED
+#define LINESEGMENT_H_INCLUDED
+#include "Point2D.h"
+
+using namespace std;
+
+class LineSegment{
+public:
+Point2D a, b;
+protected:
+
+private:
+
+};
+
+#endif // LINESEGMENT_H_INCLUDED
