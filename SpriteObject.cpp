eb45f811fe9f6528fc1b7bd041894662e910af61
sandrakuc <san.kuc.96@gmail.com>
2017-11-26 17:52:17 +0100

    SpriteObject metody lab 6 pkt 3

--- SpriteObject.cpp
+++ SpriteObject.cpp
@@ -1,13 +1,74 @@
 #include "SpriteObject.h"
 #include "BitmapHandler.h"
+#include <vector>
 #include <allegro.h>
+
+using namespace std;
 
 SpriteObject::SpriteObject(BitmapHandler bh)
 {
     this->bh = bh;
 }
 
+SpriteObject::SpriteObject(vector <BitmapHandler> bmps){
+    this->bmps = bmps;
+}
+
 SpriteObject::~SpriteObject()
 {
     //dtor
+}
+void SpriteObject::rysuj(BITMAP *buffer, int x, int y){
+    blit(bh.bmp, buffer, 0, 0, x, y, bh.x, bh.y);
+}
+
+void SpriteObject::addBmp(BitmapHandler bh){
+    bmps.push_back(bh);
+}
+
+void SpriteObject::dltBmp(){
+    bmps.pop_back();
+}
+
+void SpriteObject::rdBmp(char *path){
+    bh.readBmp(path);
+}
+
+void SpriteObject::rdBmp(vector <char *> paths){
+    for(int i=0;i<paths.size(); i++){
+        bmps[i].readBmp(paths[i]);
+    }
+}
+
+void SpriteObject::translation(){
+    if(bh.bmp != NULL){
+        if(key[KEY_UP]){
+            bh.y--;
+        }
+        if(key[KEY_DOWN]){
+            bh.y++;
+        }
+        if(key[KEY_LEFT]){
+            bh.x--;
+        }
+        if(key[KEY_RIGHT]){
+            bh.x++;
+        }
+    }
+    if(bmps[0].bmp != NULL){
+        for(int i=0;i<bmps.size();i++){
+            if(key[KEY_UP]){
+            bmps[i].y--;
+        }
+        if(key[KEY_DOWN]){
+            bmps[i].y++;
+        }
+        if(key[KEY_LEFT]){
+            bmps[i].x--;
+        }
+        if(key[KEY_RIGHT]){
+            bmps[i].x++;
+        }
+        }
+    }
 }
--- SpriteObject.h
+++ SpriteObject.h
@@ -1,16 +1,25 @@
 #ifndef SPRITEOBJECT_H
 #define SPRITEOBJECT_H
 #include <allegro.h>
+#include <vector>
 #include "BitmapHandler.h"
 
+using namespace std;
 
 class SpriteObject
 {
     public:
         BitmapHandler bh;
+        vector <BitmapHandler> bmps;
         SpriteObject(BitmapHandler bh);
+        SpriteObject(vector <BitmapHandler> bmps);
         virtual ~SpriteObject();
-
+        void rysuj(BITMAP *buffer, int x, int y);
+        void addBmp(BitmapHandler bh);
+        void dltBmp();
+        void rdBmp(char* path);
+        void rdBmp(vector <char *> paths);
+        void translation();
     protected:
 
     private:
